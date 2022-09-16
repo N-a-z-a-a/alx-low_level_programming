236586a6984c8dd10217664c124886bf49be0658
Naza <jedwin.ezeh@gmail.com>
2022-09-15 20:42:09 -0700

    Add tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Prints _putchar
+ * @a: func arg1
+ * @b: func arg2
+ * Description: when executed it multiplies two numbers
+ * Return: 0 (success)
+ */
+int mul(int a, int b)
+{
+	{
+		return (a * b);
+	}
+}
