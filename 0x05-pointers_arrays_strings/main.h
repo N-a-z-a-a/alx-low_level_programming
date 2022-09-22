c277d8e73d4640ded27f71d77db45996e176577c
Naza <jedwin.ezeh@gmail.com>
2022-09-22 03:35:49 -0700

    Add taska

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,40 @@
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+/**
+ * _putchar - prints _putchar
+ * @strin: string argument
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: string  sum of the two parameters
+ */
+
+int _putchar(char c);
+int print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
+#endif
